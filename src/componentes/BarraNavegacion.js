e874b18004fda0bbce0a44c3a2a49c6143bd02a8
decunez <decunez@espe.edu.ec>
2025-08-03 18:25:47 -0500

    Update barra de navegacion

--- src/componentes/BarraNavegacion.js
+++ src/componentes/BarraNavegacion.js
@@ -1,1 +1,24 @@
 import React from 'react';
+
+function BarraNavegacion({
+  modoOscuro,
+  alternarModoOscuro,
+  mostrarFormulario,
+}) {
+  // Manejar clic en nuevo contacto
+  const manejarNuevoContacto = () => {
+    mostrarFormulario();
+  };
+
+  // Manejar alternancia de modo oscuro
+  const manejarCambioModo = () => {
+    alternarModoOscuro();
+  };
+
+
+
+
+  
+
+}
+export default BarraNavegacion;
