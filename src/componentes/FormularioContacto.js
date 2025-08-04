3885ee793a190fc5e26fab475ca98a8555a0d864
decunez <decunez@espe.edu.ec>
2025-08-03 20:12:16 -0500

    Estados del formulario

--- src/componentes/FormularioContacto.js
+++ src/componentes/FormularioContacto.js
@@ -1,1 +1,21 @@
 import React, { useState, useEffect } from 'react';
+
+function FormularioContacto({
+  contactoEditando,
+  onGuardarContacto,
+  onCancelar,
+  modoOscuro,
+}){
+  // Estados del formulario
+  const [datosFormulario, setDatosFormulario] = useState({
+    nombre: '',
+    email: '',
+    telefono: '',
+    direccion: '',
+    foto: null,
+    fotoUrl: '',
+  });
+
+
+
+export default FormularioContacto;
