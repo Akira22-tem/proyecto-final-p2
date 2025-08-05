4272b4dea756238a8e114c2f6e86fd8ca8515b72
hjfonseca <hjfonseca@espe.edu.ec>
2025-08-04 21:19:18 -0500

    Funcion expotar a JSON

--- src/componentes/ExportarContactos.js
+++ src/componentes/ExportarContactos.js
@@ -36,4 +36,29 @@ function ExportarContactos({ contactos, modoOscuro }) {
     link.click();
     document.body.removeChild(link);
     };
+
+// Exportar a JSON 
+const exportarJSON = () => {
+    const datosExportar = {
+        agenda_guerreros: contactos,
+        total_guerreros: contactos.length,
+        fecha_exportacion: new Date().toISOString(),
+        version: '1.0',
+    };
+
+    const jsonContent = JSON.stringify(datosExportar, null, 2);
+    const blob = new Blob([jsonContent], { type: 'application/json' });
+    const link = document.createElement('a');
+    const url = URL.createObjectURL(blob);
+    link.setAttribute('href', url);
+    link.setAttribute(
+        'download',
+        `agenda_guerreros_${new Date().toISOString().split('T')[0]}.json`
+    );
+    link.style.visibility = 'hidden';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+};
+
 }
